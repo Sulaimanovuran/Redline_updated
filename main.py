8244f4212966359fe24465f0ec72d37841a0d6eb
Sulaimanovuran <sulaimanovuran@gmail.com>
2023-12-18 22:06:19 +0600

    Добавлена админ панель, сайт ГОТОВ

--- main.py
+++ main.py
@@ -23,11 +23,14 @@ class MainImages(db.Model):
 
 class MainText(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
+    title_up = db.Column(db.String(255), nullable=False)
+    title_down = db.Column(db.String(255), nullable=False)
+    title_red = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=False)
 
     def __str__(self) -> str:
         return self.title
+    
 
 class Featured(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,6 +41,13 @@ class Featured(db.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+class Discount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.Text, nullable=False)
+    image1 = db.Column(db.String(255), nullable=False, unique=True)
+    image2 = db.Column(db.String(255), nullable=False, unique=True)
+
 
 class EndWork(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -82,11 +92,28 @@ def admin_main_images():
     return render_template('admin_main_images.html', main_images=main_images)
 
 
+# @app.route('/admin/main_images/delete/<int:image_id>')
+# def delete_main_image(image_id):
+#     image_to_delete = MainImages.query.get_or_404(image_id)
+#     db.session.delete(image_to_delete)
+#     db.session.commit()
+#     return redirect(url_for('admin_main_images'))
+
+
 @app.route('/admin/main_images/delete/<int:image_id>')
 def delete_main_image(image_id):
     image_to_delete = MainImages.query.get_or_404(image_id)
+
+    # Получаем путь к файлу
+    file_path = os.path.join(image_to_delete.filename)
+
+    # Удаляем файл, если он существует
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
     db.session.delete(image_to_delete)
     db.session.commit()
+
     return redirect(url_for('admin_main_images'))
 
 
@@ -94,9 +121,11 @@ def delete_main_image(image_id):
 @app.route('/admin/main_text/', methods=['GET', 'POST'])
 def admin_main_text():
     if request.method == 'POST':
-        title = request.form.get('title')
+        title_up = request.form.get('title_up')
+        title_down = request.form.get('title_down')
+        title_red = request.form.get('title_red')
         description = request.form.get('description')
-        new_text = MainText(title=title, description=description)
+        new_text = MainText(title_up=title_up, title_down=title_down, title_red=title_red,  description=description)
         db.session.add(new_text)
         db.session.commit()
 
@@ -115,37 +144,104 @@ def delete_main_text(text_id):
 @app.route('/admin/featured/', methods=['GET', 'POST'])
 def admin_featured():
     if request.method == 'POST':
-        filename = request.form.get('filename')
-        title = request.form.get('title')
-        discount = request.form.get('discount')
-        price = request.form.get('price')
-        new_featured = Featured(filename=filename, title=title, discount=discount, price=price)
-        db.session.add(new_featured)
-        db.session.commit()
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file:
+            filename = os.path.join('images/featured/', file.filename)
+            file.save(filename)
+            title = request.form.get('title')
+            discount = request.form.get('discount')
+            price = request.form.get('price')
+            new_featured = Featured(filename=filename, title=title, discount=discount, price=price)
+            db.session.add(new_featured)
+            db.session.commit()
 
     featured_items = Featured.query.all()
     return render_template('admin_featured.html', featured_items=featured_items)
 
+
 @app.route('/admin/featured/delete/<int:featured_id>')
 def delete_featured(featured_id):
     featured_to_delete = Featured.query.get_or_404(featured_id)
+    file_path = os.path.join(featured_to_delete.filename)
+
+    # Удаляем файл, если он существует
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
     db.session.delete(featured_to_delete)
     db.session.commit()
     return redirect(url_for('admin_featured'))
+
+
+@app.route('/admin/discount/', methods=['GET', 'POST'])
+def admin_discount():
+    if request.method == 'POST':
+        file1 = request.files['file1']
+        file2 = request.files['file2']
+
+        if file1.filename == '' or file2.filename == '':
+            return redirect(request.url)
+
+        if file1 and file2:
+            image1_filename = file1.filename
+            image2_filename = file2.filename
+
+            image1 = os.path.join('images/discount/', image1_filename)
+            image2 = os.path.join('images/discount/', image2_filename)
+
+            file1.save(image1)
+            file2.save(image2)
+
+            desc = request.form.get('description')
+            new_discount = Discount(image1=image1_filename, image2=image2_filename, description=desc)
+            db.session.add(new_discount)
+            db.session.commit()
+
+    discount_items = Discount.query.all()
+    return render_template('admin_discount.html', discount_items=discount_items)
+
+
+
+@app.route('/admin/discount/delete/<int:discount_id>')
+def delete_discount(discount_id):
+    discount_to_delete = Discount.query.get_or_404(discount_id)
+    file_path1 = os.path.join('images/discount/',discount_to_delete.image1)
+    file_path2 = os.path.join('images/discount/',discount_to_delete.image2)
+
+    # Удаляем файл, если он существует
+    if os.path.exists(file_path1) or os.path.exists(file_path2):
+        os.remove(file_path1)
+        os.remove(file_path2)
+
+
+    db.session.delete(discount_to_delete)
+    db.session.commit()
+    return redirect(url_for('admin_discount'))
 
 
 # Представление для EndWork
 @app.route('/admin/end_work/', methods=['GET', 'POST'])
 def admin_end_work():
     if request.method == 'POST':
-        filename = request.form.get('filename')
-        title = request.form.get('title')
-        discount = request.form.get('discount')
-        price = request.form.get('price')
-        stars = request.form.get('stars')
-        new_end_work = EndWork(filename=filename, title=title, discount=discount, price=price, stars=stars)
-        db.session.add(new_end_work)
-        db.session.commit()
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file:
+            filename = os.path.join('images/works/', file.filename)
+            file.save(filename)
+            title = request.form.get('title')
+            discount = request.form.get('discount')
+            price = request.form.get('price')
+            stars = request.form.get('stars')
+            new_end_work = EndWork(filename=filename, title=title, discount=discount, price=price, stars=stars)
+            db.session.add(new_end_work)
+            db.session.commit()
 
     end_work_items = EndWork.query.all()
     return render_template('admin_end_work.html', end_work_items=end_work_items)
@@ -153,6 +249,11 @@ def admin_end_work():
 @app.route('/admin/end_work/delete/<int:end_work_id>')
 def delete_end_work(end_work_id):
     end_work_to_delete = EndWork.query.get_or_404(end_work_id)
+    file_path = os.path.join(end_work_to_delete.filename)
+    # Удаляем файл, если он существует
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
     db.session.delete(end_work_to_delete)
     db.session.commit()
     return redirect(url_for('admin_end_work'))
@@ -162,13 +263,20 @@ def delete_end_work(end_work_id):
 @app.route('/admin/employee/', methods=['GET', 'POST'])
 def admin_employee():
     if request.method == 'POST':
-        filename = request.form.get('filename')
-        title = request.form.get('title')
-        description = request.form.get('description')
-        stars = request.form.get('stars')
-        new_employee = Employee(filename=filename, title=title, description=description, stars=stars)
-        db.session.add(new_employee)
-        db.session.commit()
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file:
+            filename = os.path.join('images/employee/', file.filename)
+            file.save(filename)
+            title = request.form.get('title')
+            description = request.form.get('description')
+            stars = request.form.get('stars')
+            new_employee = Employee(filename=filename, title=title, description=description, stars=stars)
+            db.session.add(new_employee)
+            db.session.commit()
 
     employees = Employee.query.all()
     return render_template('admin_employee.html', employees=employees)
@@ -176,6 +284,10 @@ def admin_employee():
 @app.route('/admin/employee/delete/<int:employee_id>')
 def delete_employee(employee_id):
     employee_to_delete = Employee.query.get_or_404(employee_id)
+    file_path = os.path.join(employee_to_delete.filename)
+    # Удаляем файл, если он существует
+    if os.path.exists(file_path):
+        os.remove(file_path)
     db.session.delete(employee_to_delete)
     db.session.commit()
     return redirect(url_for('admin_employee'))
@@ -190,7 +302,9 @@ def index():
     featureds = Featured.query.all()
     works = EndWork.query.all()
     employees = Employee.query.all()
-    return render_template('index.html', image_names=image_names, featureds=featureds, works=works, employees=employees) #['main-image1.png','main-image2.png','main-image3.png','main-image4.png']
+    main_texts = MainText.query.all()
+    discounts = Discount.query.all()
+    return render_template('index.html', image_names=image_names, featureds=featureds, works=works, employees=employees, main_texts=main_texts, discounts=discounts) #['main-image1.png','main-image2.png','main-image3.png','main-image4.png']
 
 @app.route('/images/<path:filename>')
 def get_image(filename):
@@ -259,79 +373,3 @@ if __name__ == '__main__':
 
 #https://www.youtube.com/watch?v=b7eJQSHhuO8&list=PL07efmqYWHZ_cxA1GvuXQMA-VYk8dhuiv&index=3
     
-
-
-
-# @app.route('/upload', methods=['GET','POST'])
-# def upload():
-#     if 'file' not in request.files:
-#         return render_template('upload.html')
-
-#     file = request.files['file']
-
-#     if file.filename == '':
-#         return redirect(request.url)
-
-#     if file:
-#         filename = os.path.join('images/featured/', file.filename)
-#         file.save(filename)
-#         title = request.form['title']
-#         discount = request.form['discount']
-#         price = request.form['price']
-#         new_image = Featured(filename=file.filename, title=title, discount=discount, price=price)
-#         db.session.add(new_image)
-#         db.session.commit()
-
-#         return redirect(url_for('index'))
-    
-
-# @app.route('/works', methods=['GET','POST'])
-# def upload_works():
-#     if 'file' not in request.files:
-#         return render_template('works.html')
-
-#     file = request.files['file']
-
-#     if file.filename == '':
-#         return redirect(request.url)
-
-#     if file:
-#         filename = os.path.join('images/works/', file.filename)
-#         file.save(filename)
-        
-#         title = request.form['title']
-#         discount = request.form['discount']
-#         price = request.form['price']
-#         stars = int(request.form['stars']) if not isinstance(request.form['stars'], int) else request.form['stars']
-        
-#         new_image = EndWork(filename=file.filename, title=title, discount=discount, price=price, stars=stars)
-#         db.session.add(new_image)
-#         db.session.commit()
-
-#         return redirect(url_for('index'))
-    
-
-# @app.route('/employee', methods=['GET','POST'])
-# def upload_employee():
-#     if 'file' not in request.files:
-#         return render_template('employee.html')
-
-#     file = request.files['file']
-
-#     if file.filename == '':
-#         return redirect(request.url)
-
-#     if file:
-#         filename = os.path.join('images/employee/', file.filename)
-#         file.save(filename)
-        
-#         title = request.form['title']
-#         description = request.form['description']
-
-#         stars = int(request.form['stars']) if not isinstance(request.form['stars'], int) else request.form['stars']
-        
-#         new_image = Employee(filename=file.filename, title=title, description=description, stars=stars)
-#         db.session.add(new_image)
-#         db.session.commit()
-
-#         return redirect(url_for('index'))
